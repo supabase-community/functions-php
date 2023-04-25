efb79bab003d72a27ba828b8d66b4e2738675eed
Norm Santos <santnorm@gmail.com>
2023-04-24 23:25:26 -0600

    Fixing one of the examples.

--- examples/invoke-a-function/basic-invocation.php
+++ examples/invoke-a-function/basic-invocation.php
@@ -3,12 +3,7 @@
 include __DIR__.'../../header.php';
 use Supabase\Functions\FunctionsClient;
 
-$scheme = 'https';
-$domain = 'functions.supabase.co';
+$client = new FunctionsClient($reference_id, $api_key);
 
-$client = new FunctionsClient($reference_id, $api_key, [], $domain, $scheme);
-
-$response = $client->invoke('hello-world', [
-	'body'                => ['name'=>'Supabase'],
-]);
+$response = $client->invoke('hello-world', ['name'=>'Supabase']);
 print_r($response);
